779c032f623a529bd2601154871ac87ebeb92949
alanduarte90 <alanduarte90@gmail.com>
2019-03-10 14:40:54 -0300

    Change rounting logic to a separated file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import routing from './config/routing.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
