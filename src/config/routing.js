d33f17e62fed87f96d5754c94a5ef5919ba50854
alanduarte90 <alanduarte90@gmail.com>
2019-03-10 23:56:53 -0300

    Add rounting file

--- src/config/routing.js
+++ src/config/routing.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import React from 'react'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import App from '../App';
+import NotFound from '../components/NotFount'
+import Auth from '../components/auth/Auth'
 
 const routing = (
     <Router>
+        <Switch>
             <Route exact path="/" component={App} />
+            <Route path="/auth" component={Auth} />
+            <Route component={NotFound} />
+        </Switch>
     </Router>
 )
 
