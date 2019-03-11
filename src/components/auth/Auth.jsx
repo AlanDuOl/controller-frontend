cd78fca307defed095ab4e0f138463b6f8803eb8
alanduarte90 <alanduarte90@gmail.com>
2019-03-10 23:54:34 -0300

    Start auth page

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
+import '../../css/Auth.css'
 
 class Auth extends Component {
+	constructor(props){
+		super(props)
+
+		this.state = {
+			loginPage: true
+		}
+
+		this.togglePage = this.togglePage.bind(this)
+	}
+
+	togglePage(event) {
+		let currentState = this.state.loginPage
+		this.setState({ loginPage: !currentState })
+	}
+
 	render() {
 		return (
-			<div></div>
+			<div className="container">
+					{this.state.loginPage ? null : <input type="text" placeholder="Nome"/> }
+					<input name="email" type="text" placeholder="E-mail"/>
+					<input name="password" type="password" placeholder="Senha"/>
+					{this.state.loginPage ? null : <input type="password" placeholder="Confirme a senha!"/> }
+					{this.state.loginPage ? <button>Entrar</button> : <button>Registrar</button>}
+					<a href onClick={this.togglePage}>						
+						{this.state.loginPage ? <span className="auth-link">Não tem cadastro? Registre-se aqui!</span> : <span className="auth-link">Já tem cadastro? Acesse o login!</span> }
+					</a>
+			</div>
 		)
 	}
 }
