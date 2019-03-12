451a6a3e38b50861fd664ea820ee6dcdd417882a
alanduarte90 <alanduarte90@gmail.com>
2019-03-11 23:33:18 -0300

    Add new route

--- src/config/routing.js
+++ src/config/routing.js
@@ -3,12 +3,14 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import App from '../App';
 import NotFound from '../components/NotFount'
 import Auth from '../components/auth/Auth'
+import Insert from '../components/transaction/Insert'
 
 const routing = (
     <Router>
         <Switch>
             <Route exact path="/" component={App} />
             <Route path="/auth" component={Auth} />
+            <Route path="/transactions/insert" component={Insert} />
             <Route component={NotFound} />
         </Switch>
     </Router>
