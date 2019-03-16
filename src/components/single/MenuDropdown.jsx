a050ed8e33de06b11ba1d8fca7f1e04f52b33fcf
alanduarte90 <alanduarte90@gmail.com>
2019-03-15 23:39:53 -0300

    Changes: adding component Dropdown

--- src/components/single/MenuDropdown.jsx
+++ src/components/single/MenuDropdown.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Nav, NavDropdown } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
+import Dropdown from '../widget/Dropdown'
 import { Link } from 'react-router-dom'
 
 import '../../css/MenuDropdown.css'
@@ -27,21 +28,7 @@ class MenuDropdown extends Component {
 				<Nav className={this.state.active ? "mr-auto menu-dropdown-content toggle-menu" : "mr-auto menu-dropdown-content"}>
 					<div className={this.state.active ? "menu-dropdown toggle-menu" : "menu-dropdown"}>
 						<Link className="link-home" to="/" onClick={this.toggleMenu}>Home</Link>
-						<NavDropdown title="Usuário" id="basic-nav-dropdown">
-						</NavDropdown>
-						<NavDropdown title="Operações" id="basic-nav-dropdown">
-							<ul className="menu-items" onClick={this.toggleMenu}>
-								<Link className="link-item" to="/transactions/insert">Inserir</Link>
-								<Link className="link-item" to="/transactions/view">Visualizar</Link>
-							</ul>
-						</NavDropdown>
-						<NavDropdown title="Relatórios" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1" onClick={this.toggleMenu}>Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2" onClick={this.toggleMenu}>Another action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3" onClick={this.toggleMenu}>Something</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4" onClick={this.toggleMenu}>Separated link</NavDropdown.Item>
-						</NavDropdown>
+						<Dropdown title="Operações" links={[{name: "Inserir", path: "/transactions/insert"}, {name: "Visualizar", path: "/transactions/view"}]}/>
 					</div>
 				</Nav>
 			</div>
