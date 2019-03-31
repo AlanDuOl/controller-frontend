34a651a1cd8be69edcc71d6cb3e982c5027aa5cd
alanduarte90 <alanduarte90@gmail.com>
2019-03-31 15:56:50 -0300

    Start to implement insert transactions logic

--- src/components/widget/MyForm.jsx
+++ src/components/widget/MyForm.jsx
@@ -1,24 +1,50 @@
 import React, { Component } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import axios from 'axios'
+import { baseApiUrl } from '../../global'
 
 class MyForm extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.data = { userId: this.props.user.id }
+    }
+
+    save = () => {
+        console.log(this.data)
+        axios.post(`${baseApiUrl}/transactions/insert`, this.data)
+            .then(res => console.log('user saved: ', res.data))
+            .catch(err => console.log(err))
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.save()
+    }
+
+    handleChange = event => {
+        this.data[event.target.name] = event.target.value
+    }
 
     render(){
         return (
             <div id="form">
-                <h4 className="header">Inserir transação</h4>
-                <Form className="insert-form">
+                <label id="form-header">Inserir transação</label>
+                <Form id="insert-form" onSubmit={this.handleSubmit}>
                     <Form.Row className="form-row">
                         <Form.Group className="col-width">
                             <Form.Label>Natureza</Form.Label>
-                            <Form.Control as="select">
+                            <Form.Control as="select" name="type" onChange={this.handleChange}>
+                                <option>Selecione...</option>
                                 <option>Receita</option>
                                 <option>Despesa</option>
                             </Form.Control>
                         </Form.Group>
                         <Form.Group className="col-width">
                             <Form.Label>Tipo</Form.Label>
-                            <Form.Control as="select">
+                            <Form.Control as="select" name="transaction" onChange={this.handleChange}>
+                                <option>Selecione...</option>
                                 <option>Compra</option>
                                 <option>Venda</option>
                                 <option>Salário</option>
@@ -27,19 +53,19 @@ class MyForm extends Component {
                         </Form.Group>
                         <Form.Group className="col-width">
                             <Form.Label>Descrição da transação</Form.Label>
-                            <Form.Control type="text" placeholder="Ex: compra produto xpto..." />
+                            <Form.Control type="text" placeholder="Ex: compra produto xpto..." name="description" onChange={this.handleChange} />
                         </Form.Group>
                         <Form.Group className="col-width">
                             <Form.Label>Data</Form.Label>
-                            <Form.Control type="date" />
+                            <Form.Control type="date" name="transactionDate" onChange={this.handleChange} />
                         </Form.Group>
                         <Form.Group className="col-width">
                             <Form.Label>Valor</Form.Label>
-                            <Form.Control type="number" placeholder="R$..." />
+                            <Form.Control type="number" placeholder="R$..." name="amount" onChange={this.handleChange} />
                         </Form.Group>
                     </Form.Row>
                     <div id="form-btn-container">
-                        <Button onSubmit={this.props.save} className="col-width" variant="primary" type="submit" id="form-btn">
+                        <Button className="col-width" variant="primary" type="submit" id="form-btn">
                             Salvar
                         </Button>
                     </div>
