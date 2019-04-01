ec91bc5ab46f693641cb82dc7a882c652561c78b
alan <alanduarte90@gmail.com>
2019-04-01 19:03:41 -0300

    Redirect on login

--- src/config/PublicRoutes.js
+++ src/config/PublicRoutes.js
@@ -3,10 +3,11 @@ import Auth from '../components/auth/Auth'
 import { Route } from 'react-router-dom'
 
 class PublicRoutes extends Component {
+	
     render(){
         return (
             <div>
-                <Route path="/auth" component={Auth} />
+                <Route path="*" component={Auth} />
             </div>
         )
     }
