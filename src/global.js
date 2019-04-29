87582b3ecf1efd24622cecad2785d395116bbe5f
alan <alanduarte90@gmail.com>
2019-04-29 19:11:25 -0300

    Add fields const to global.js

--- src/global.js
+++ src/global.js
@@ -1,4 +1,5 @@
 export const userKey = '__controllerUser'
 export const baseApiUrl = 'http://localhost:4000'
+export const fields = ['type', 'transaction', 'description', 'amount', 'transactionDate']
 
-export default { baseApiUrl, userKey }
+export default { baseApiUrl, userKey, fields }
