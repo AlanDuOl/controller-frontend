b11b52d43f6c503ac2826405588ec8b43144a924
alanduarte90 <alanduarte90@gmail.com>
2019-05-01 11:05:41 -0300

    View: start to enable edit transaction

--- src/components/transaction/View.jsx
+++ src/components/transaction/View.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { baseApiUrl, fields } from '../../global'
+import { baseApiUrl } from '../../global'
 import { connect } from 'react-redux'
 import '../../css/View.css'
 import ViewForm from '../widget/ViewForm'
@@ -11,8 +11,12 @@ class View extends Component {
         super(props)
 
         this.state = {
-            transactions: []
+            transactions: [],
+            editData: [],
+            edit: false
         }
+
+        this.fields = ['id', 'type', 'transaction', 'description', 'amount', 'transactionDate']
     }
 
     renderTransactions = () => {
@@ -26,20 +30,29 @@ class View extends Component {
 			})
             .catch(err => console.log('didnt load data from server: ', err))
     }
+
+    enableEdit = event => {
+        let el = event.target.parentElement.parentElement.firstChild.children
+        let editValues = []
+        for(let i = 0; i < el.length; i++){
+            editValues.push(el[i].innerHTML)
+        }
+        this.setState({ edit: true, editData: editValues })
+    }
 	
 	loadComponents = () => {
 		let rows = []
         const val = this.state.transactions
 		for(let index in val){
 			let data = []
-			for(let field in fields){
-                if(Number(field) === fields.length-1){
-                    data.push(<span className="row-data" type={typeof val[index][fields[field]]} key={field+"-"+index}>{`${val[index][fields[field]].slice(8,10)}-${val[index][fields[field]].slice(5,7)}-${val[index][fields[field]].slice(0,4)}`}</span>)
+			for(let field in this.fields){
+                if(Number(field) === this.fields.length-1){
+                    data.push(<span className="row-data" type={typeof val[index][this.fields[field]]} key={field+"-"+index}>{`${val[index][this.fields[field]].slice(8,10)}-${val[index][this.fields[field]].slice(5,7)}-${val[index][this.fields[field]].slice(0,4)}`}</span>)
                 } else {
-                    data.push(<span className="row-data" type={typeof val[index][fields[field]]} key={field+"-"+index}>{val[index][fields[field]]}</span>)
+                    data.push(<span className="row-data" type={typeof val[index][this.fields[field]]} key={field+"-"+index}>{val[index][this.fields[field]]}</span>)
                 }
 			}
-			const btns = <div className="row-btns"><button id="edit-btn"></button><button id="delete-btn"></button></div>
+			const btns = <div className="row-btns"><button id="edit-btn" onClick={this.enableEdit}></button><button id="delete-btn"></button></div>
 			rows.push(<div className="row-container" key={index}><div className="row-fields">{data}</div>{btns}</div>)
 		}
 		return rows
@@ -50,13 +63,13 @@ class View extends Component {
     }
     
     log = () => {
-        console.log()
+        console.log(this.state.transactions)
     }
 
     render(){
         return (
             <div id="view-container">
-				<ViewForm user={this.props.user} />
+				<ViewForm user={this.props.user} edit={this.state.edit} editData={this.state.editData} />
 				{this.loadComponents()}
 			</div>
         )
