78cf968ab065eb1f286a983aa41006ef095b3cec
alanduarte90 <alanduarte90@gmail.com>
2019-05-05 22:02:52 -0300

    Add data to anable InsertTransactions component

--- src/components/transaction/Insert.jsx
+++ src/components/transaction/Insert.jsx
@@ -2,18 +2,45 @@
 import '../../css/Insert.css'
 import MyForm from '../widget/MyForm'
 import { connect } from 'react-redux'
+import InsertTransactions from '../widget/InsertTransactions'
+import axios from 'axios'
+import { baseApiUrl } from '../../global'
 
 class Insert extends Component {
 
+    constructor(props){
+        super(props)
+
+        this.state = { transactions: [] }
+        this.fields = ['type', 'transaction', 'description', 'amount', 'transactionDate']
+        this.limit = 5
+    }
+
     table = {
-        head: ['Natureza', 'Tipo', 'Descrição', 'Valor', 'Data'],
-        header: 'Transações recentes'
+        head: ['Natureza', 'Tipo', 'Descrição', 'Valor', 'Data']
+    }
+
+    getData = () => {
+        axios.get(`${baseApiUrl}/transactions/${this.props.user.id}/${this.limit}`)
+            .then(res => {
+				this.setState({ transactions: res.data })
+			})
+            .catch(err => console.log('didnt load data from server: ', err))
+    }
+
+    reloadData = () => {
+        this.getData()
+    }
+
+    componentDidMount() {
+		this.getData()
     }
 
     render() {
         return (
             <div id="insert-container">   
-                <MyForm user={this.props.user} />
+                <MyForm user={this.props.user} reloadData={this.reloadData} />
+                <InsertTransactions transactions={this.state.transactions} fields={this.fields} />
             </div>
         )
     }
