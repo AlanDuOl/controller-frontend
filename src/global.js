b57f0ca160e95e182d3cce528e0add59cb656829
alanduarte90 <alanduarte90@gmail.com>
2019-11-06 15:12:26 +0000

    New host for remote heroku

--- src/global.js
+++ src/global.js
@@ -1,5 +1,6 @@
 export const userKey = '__controllerUser'
-export const baseApiUrl = 'http://localhost:4000'
+export const baseApiUrl = `postgres://yalbfainbznvmz:f74bba1252f3e5d7bfbead10cb57f294143e2538f600a764408086dc506e8e70@ec2-54-75-249-16.eu-west-1.compute.amazonaws.com:5432/dat6vkuviocndi
+`
 export const fields = ['type', 'transaction', 'description', 'amount', 'transactionDate']
 
 export default { baseApiUrl, userKey, fields }
