279f0ef274ea0bed64aeb36b9b4fc0d2f3f75429
alanduarte90 <alanduarte90@gmail.com>
2019-11-06 16:25:52 +0000

    Fix typo error

--- src/global.js
+++ src/global.js
@@ -1,6 +1,5 @@
 export const userKey = '__controllerUser'
-export const baseApiUrl = `postgres://yalbfainbznvmz:f74bba1252f3e5d7bfbead10cb57f294143e2538f600a764408086dc506e8e70@ec2-54-75-249-16.eu-west-1.compute.amazonaws.com:5432/dat6vkuviocndi
-`
+export const baseApiUrl = `postgres://yalbfainbznvmz:f74bba1252f3e5d7bfbead10cb57f294143e2538f600a764408086dc506e8e70@ec2-54-75-249-16.eu-west-1.compute.amazonaws.com:5432/dat6vkuviocndi`
 export const fields = ['type', 'transaction', 'description', 'amount', 'transactionDate']
 
 export default { baseApiUrl, userKey, fields }
