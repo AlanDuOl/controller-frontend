4004f650695c85b1a97191c45aa91eff1c80758d
alanduarte90 <alanduarte90@gmail.com>
2019-11-12 01:13:23 +0000

    Set api url

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 export const userKey = '__controllerUser'
-export const baseApiUrl = 'https://dashboard.heroku.com/apps/controlle-api'
+export const baseApiUrl = 'https://controlle-api.herokuapp.com'
 export const fields = ['type', 'transaction', 'description', 'amount', 'transactionDate']
 
 export default { baseApiUrl, userKey, fields }
