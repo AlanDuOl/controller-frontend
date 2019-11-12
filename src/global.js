8aa73b5280e60cdd4394bbe080d273cd44bbda1b
alanduarte90 <alanduarte90@gmail.com>
2019-11-12 07:10:48 +0000

    Add dev url as comment

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 export const userKey = '__controllerUser'
-export const baseApiUrl = 'https://controlle-api.herokuapp.com'
+export const baseApiUrl =  "https://controlle-api.herokuapp.com" //http://localhost:4000
 export const fields = ['type', 'transaction', 'description', 'amount', 'transactionDate']
 
 export default { baseApiUrl, userKey, fields }
