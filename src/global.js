7dfa29562f79689012e5d154d486992825abbf96
alanduarte90 <alanduarte90@gmail.com>
2019-11-15 14:29:47 +0000

    Switch to production

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 export const userKey = '__controllerUser'
-export const baseApiUrl =  "http://localhost:4000" //http://localhost:4000 https://controlle-api.herokuapp.com
+export const baseApiUrl =  "https://controlle-api.herokuapp.com" //http://localhost:4000 https://controlle-api.herokuapp.com
 export const fields = ['type', 'transaction', 'description', 'amount', 'transactionDate']
 
 export default { baseApiUrl, userKey, fields }
